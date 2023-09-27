0c500f7c24a464da2de3f2a4050fc78ea29743bc
Nathaniel Richley <74015928+Nathaniel-Richley@users.noreply.github.com>
2023-09-27 08:46:12 -0400

    Add files via upload
    task 1

---
+++ python_fundamentals_exam.py
@@ -0,0 +1,28 @@
+intro = "Hello World"
+print(intro)
+first_name = input("what is your first name")
+print(first_name)
+
+age = int(input("What is your age")) 
+temp = float(input("What is the Temperature"))
+temperature = 75
+temp = 32
+
+if temp > temperature:
+  print("Too cold to go outside.")
+else:
+  print("You can go outside.")
+
+addition = age + temp
+subtraction = age - temp
+Multiplication = age * temp 
+Division = age // temp 
+square = age ** temp 
+
+print (addition)
+print (subtraction)
+print (Multiplication)
+print (Division)
+print (square)
+
+  
