4aac104d81a461ebcda2445fed0c838133fdd28a
Nathaniel Richley <74015928+Nathaniel-Richley@users.noreply.github.com>
2023-09-27 09:09:31 -0400

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,10 @@
+number= int(input("What is your first number"))
+if number <0: 
+    print("you have a negative number")
+
+elif number >0: 
+    print("You have a positive number")
+
+elif number == 0:
+    print("Your number is zero") 
+    
